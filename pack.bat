3e3c067a0d3d2ceffdef7a279f3b0a5e93075e77
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-03-13 14:14:22 +0900

    add pack.bat

---
+++ pack.bat
@@ -0,0 +1,9 @@
+@echo on
+set MYDIR=CBTHooker-1.0-bin
+if not exist %MYDIR% mkdir %MYDIR%
+copy /Y dll32.dll %MYDIR%
+copy /Y dll64.dll %MYDIR%
+copy /Y watcher32.exe %MYDIR%
+copy /Y watcher64.exe %MYDIR%
+copy /Y CBTHooker.exe %MYDIR%
+copy /Y README.txt %MYDIR%
